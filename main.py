9ea6f860d02229431cd975be7a76232f57033ba5
abhash <abhashjha16@gmail.com>
2018-05-21 14:55:25 -0500

    Added skeleton code for main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+flag=True
+
+facefeatures = cv2.
+eyefeatures = cv2.
+
+video = cv2.VideoCapture(0)
+while flag:
+    if oxFF = 'q':
+        flag=False
+
+    faceCord = facefeatures.
+    for cords  in faceCord:
+
+
+video.release()
+cv2.destroyAllWindows()
+
